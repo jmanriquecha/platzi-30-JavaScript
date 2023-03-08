a95bfce016ae0b1dc5a535ef68a823d5e546a3c4
Jorge Manrique <georgemanc.28@gmail.com>
2023-03-08 11:34:58 -0500

    Solución al reto del día 1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Día 1
+
+let solucion1 = document.getElementById("solucion1")
+
+function solution(valor){
+    return typeof(valor);
+}
+
+solucion1.innerHTML += "<p> <strong>Tipo de dato:</strong> " + solution(1) + "</p>";
+solucion1.innerHTML += "<p> <strong>Tipo de dato:</strong> " + solution("Dieguillo") + "</p>";
+solucion1.innerHTML += "<p> <strong>Tipo de dato:</strong> " + solution(true) + "</p>";
